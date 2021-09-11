94b42a35c6cfd31237230a0347a954c1a123a1be
Shoaib <shoaib.keev@gmail.com>
2021-09-11 20:58:44 +0530

    Adding second file

---
+++ annotations_python.py
@@ -0,0 +1,16 @@
+name = ""
+
+
+def method_name():
+    print("ello sd sd s d")
+    print("ll")
+    print(">>>")
+
+
+method_name()
+
+
+def add(x: int, y: int) -> None:
+    print("Addition is ",x+y)
+
+add()
